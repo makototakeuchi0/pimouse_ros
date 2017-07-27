5d39264b5e1ba292421301bf8432e263064ba3c6
Makoto Takeuchi <1143013@stu.hus.ac.jp>
2017-07-27 05:31:50 +0000

    install ros at travisCI

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -xve
+
+#required packeges
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospkg
+
+#ros install
+cd ..
+git clone http://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubuntu14.04_server
+bash ./setup0.bash
+bash ./setup1.bash
+
+#catkin setup
+mkdir -p ~/catkin_ws/scr
+cd ~/catkin_ws/scr
+source /opt/ros/indigo/setup.bash
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
